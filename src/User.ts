e1bf241fb6e8b60970ffdd86ae46dcc5fc99f0e2
jhawkins11 <josiah.c.hawkins@gmail.com>
2023-04-05 13:51:32 -0500

    add user event methods

--- src/User.ts
+++ src/User.ts
@@ -1,9 +1,13 @@
 interface UserProps {
   name?: string;
   age?: number;
+  on?: (eventName: string, callback: Callback) => void;
 }
 
+type Callback = () => {};
+
 export class User {
+  events: { [key: string]: Callback[] } = {};
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -12,5 +16,21 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callback) => {
+      callback();
+    });
   }
 }
